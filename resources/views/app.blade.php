f9404a413a126aca4475bf9d2426054a1cbb7a32
rusty0515 <decolongonrusty23@gmail.com>
2026-05-08 13:49:55 +0800

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -48,7 +48,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @routes
-    @viteReactRefresh
+    {{-- @viteReactRefresh --}}
     {{-- @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"]) --}}
     @vite('resources/js/app.tsx')
     @inertiaHead
